d2a54af6a164c6245b8f8bcbd92cf50d1dea2da2
Anushka Ojha <ao8809@srmist.edu.in>
2026-03-12 15:46:42 +0530

    UC13:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
-// Palindrome service class
-class PalindromeChecker {
+public class UseCase13PalindromeCheckerApp {
 
-    // Method to check palindrome
-    public boolean checkPalindrome(String str) {
+    // Method 1: Two Pointer Method
+    public static boolean twoPointerCheck(String str) {
 
         int start = 0;
         int end = str.length() - 1;
@@ -21,9 +20,44 @@ class PalindromeChecker {
 
         return true;
     }
-}
 
-public class UseCase11PalindromeCheckerApp {
+    // Method 2: Stack Based Check
+    public static boolean stackCheck(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Method 3: Deque Based Check
+    public static boolean dequeCheck(String str) {
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (char c : str.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
 
@@ -32,17 +66,30 @@ public class UseCase11PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Create object of PalindromeChecker
-        PalindromeChecker checker = new PalindromeChecker();
+        // Two Pointer Performance
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
 
-        // Call method
-        boolean result = checker.checkPalindrome(input);
+        // Stack Performance
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(input);
+        long end2 = System.nanoTime();
 
-        if (result) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
+        // Deque Performance
+        long start3 = System.nanoTime();
+        boolean result3 = dequeCheck(input);
+        long end3 = System.nanoTime();
+
+        System.out.println("\nPalindrome Results:");
+        System.out.println("Two Pointer Method: " + result1);
+        System.out.println("Stack Method: " + result2);
+        System.out.println("Deque Method: " + result3);
+
+        System.out.println("\nExecution Time (nanoseconds):");
+        System.out.println("Two Pointer Method: " + (end1 - start1));
+        System.out.println("Stack Method: " + (end2 - start2));
+        System.out.println("Deque Method: " + (end3 - start3));
 
         sc.close();
     }
